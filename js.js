99fb4e1b952fb9f448b1985337ae0c4bc77cde5f
temirov02 <humoyuntemirov02@gmail.com>
2021-08-02 16:46:33 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+
+
+//     $('#add').click(function () {
+//         var res = "<a href='https://" + $('#link').val() + "'>" + $('#name').val() + "</a>";
+//         $("#results").append(res);
+//         $('#name').val('');
+//         $('#link').val('');
+//     });
+//     $('#clear').click(function () {
+//         $("#results a").remove();
+//     })
+    
+// });
+
+// document.getElementById('add').addEventListener('click', function () {
+//     var link = document.getElementById('link').value;
+//     var name = document.getElementById('name').value;
+
+    
+//     document.getElementById('result').append("<input value='"+link+"' type='text'><input value='"+name+"' type='text'>");
+//     link ='';
+//     name ='';
+    
+    
+// });
